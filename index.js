9928b1ca3561e03d24642b53810a766e4ae04471
Luke Gregurich <lgreguri@purdue.edu>
2018-05-14 14:01:04 -0400

    synced js file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('It works!')
+
+const changeHeading = function(){
+  const heading = document.querySelector('h1')
+  heading.textContent = 'It is changed'
+}
+
+const button = document.querySelector('button')
+button.addEventListener('click', changeHeading)
