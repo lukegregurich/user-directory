2e608d901c4d22d546fc698b02abdea08a2c9405
Luke Gregurich <lgreguri@purdue.edu>
2018-05-15 09:52:24 -0400

    Finished part3

--- part3.js
+++ part3.js
@@ -1,6 +1,8 @@
 console.log('It works!')
 
 const changeHeading = function(ev){
+  ev.preventDefault() //break point to pause execution, 
+  //stop the page from refreshing
   const heading = document.querySelector('h1')
   const input = document.querySelector('input')
   heading.textContent = input.value
