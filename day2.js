ffc608ecd4d16d2dad788c1b5d2ea6990bcf0874
Luke Gregurich <lgreguri@purdue.edu>
2018-05-15 10:50:59 -0400

    Add each username in the paragraph

--- day2.js
+++ day2.js
@@ -1,14 +1,15 @@
 console.log('It works!')
 
+const form = document.querySelector('form#userForm')
+
 const changeHeading = function(ev){
   ev.preventDefault() //break point to pause execution, 
   //stop the page from refreshing
-  const userName = formEntry.userName.value
-  const heading = document.querySelector('h1')
+  const form = ev.target
+  const userName = form.userName.value
   const users = document.querySelector('#users')
-  users.textContent += ' ' + userName
-  formEntry.userName.value = ''
+  users.innerHTML += userName
+  form.userName.value = ''
 }
 
-const formEntry = document.querySelector('form#userForm')
-formEntry.addEventListener('submit', changeHeading)
+form.addEventListener('submit', changeHeading)
